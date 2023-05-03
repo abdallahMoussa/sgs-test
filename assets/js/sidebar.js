f627f216a6c994a6b0cf1d135bad582f491d09c3
Abdallah Moussa <55410420+abdallahMoussa@users.noreply.github.com>
2023-05-04 02:57:46 +0300

    Add files via upload

---
+++ assets/js/sidebar.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  let isMobile = window.matchMedia('only screen and (max-width: 760px)').matches;
+
+  if (isMobile) {
+    $('.side-bar').css('display', 'none');
+    $('.side-toogleer').click(function () {
+      $('.side-bar').css('display', 'block');
+    });
+    $('.bar-closer').click(function () {
+      $('.side-bar').css('display', 'none');
+    });
+  } else {
+    $('.side-toogleer').click(function () {
+      $('.side-bar').css('right', '0%');
+    });
+    $('.bar-closer').click(function () {
+      $('.side-bar').css('right', '-50%');
+    });
+  }
+
+  // togglers
+  $('.nav-one').click(function () {
+    $('.nav-one-content').slideToggle();
+  });
+  $('.nav-two').click(function () {
+    $('.nav-two-content').slideToggle();
+  });
+  $('.nav-three').click(function () {
+    $('.nav-three-content').slideToggle();
+  });
+  $('.nav-four').click(function () {
+    $('.nav-four-content').slideToggle();
+  });
+});
